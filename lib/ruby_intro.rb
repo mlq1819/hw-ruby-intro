5b374aa1e9f779a4591f0b3a49773990eeb338a8
Michael Quinn <mquinn5@binghamton.edu>
2019-02-06 02:16:08 +0000

    finished part 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -21,36 +21,58 @@ def sum(array)
 end
 
 def max_2_sum(arr)
-  largest = 0
-  second_largest = 0
+  
+  if arr.length < 2
+    if arr.length == 0
+      return 0
+    else
+      return arr[0]
+    end
+  else
+    largest = arr[0]
+    second_largest = arr[1]
+    if second_largest > largest
+      largest = arr[1]
+      second_largest = arr[0]
+    end
+    for i in 2..arr.length do
+      begin
+        raise NotAnIntegerError,
+          'Expected argument must be scalar whole number' unless
+          arr[i].is_a?(Integer)
+        if arr[i] > largest
+          second_largest = largest
+          largest = arr[i]
+        elsif arr[i] > second_largest
+          second_largest = arr[i]
+        end
+        rescue NotAnIntegerError => exception
+        "#{exception.class}: #{exception.message}"
+      end
+    end
+    return largest + second_largest
+  end
+end
+
+def sum_to_n?(arr, n)
   for i in 0..arr.length do
     begin
       raise NotAnIntegerError,
         'Expected argument must be scalar whole number' unless
-        array[i].is_a?(Integer)
-      if arr[i] > largest
-        second_largest = largest
-        largest = arr[i]
-      elsif arr[i] > second_largest
-        second_largest = arr[i]
+        arr[i].is_a?(Integer)
+      for j in i+1..arr.length do
+        raise NotAnIntegerError,
+          'Expected argument must be scalar whole number' unless
+          arr[j].is_a?(Integer)
+        if arr[i] + arr[j] == n
+          return true
+        end
       end
-      rescue NotAnIntegerError => exception
+    rescue NotAnIntegerError => exception
       "#{exception.class}: #{exception.message}"
     end
   end
-  return largest + second_largest
-end
-
-def sum_to_n(arr, n)
-  x = 0
-  until x = arr.length || arr[x]=n
-   x = x+1
-  end
-  if x < arr.length
-    return true
-  else
-    return false
-  end
+  return false
 end
 
 # Part 2
